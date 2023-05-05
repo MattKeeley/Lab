a6caa677d1b7c82e7977457d64918015f7ba5526
Matt Keeley <keeley55@me.com>
2023-05-05 15:34:45 -0700

    feat: more users and fully working

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, jsonify, request
+import hashlib
 
 app = Flask(__name__)
 
 # Pretend this is a databse :P
 users = [
-    {'id': 1, 'name': 'Alice', 'email': 'alice@example.com', 'password': 'bbb'},
-    {'id': 2, 'name': 'Bob', 'email': 'bob@example.com', 'password': 'aaa'},
-    {'id': 3, 'name': 'Charlie', 'email': 'charlie@example.com', 'password': 'ccc'}
+    {'id': 0, 'name': 'admin', 'email': 'admin@example.com',
+     'password': '2355d80c210fded577805e0b984810fe6605a51bb790b18d35092121250653ae'},
+    {'id': 1, 'name': 'Matt', 'email': 'matt@example.com',
+        'password': '50eb299b46fe70fb31b22d195410c0c2b6b959a8beebd8c2c080f428b90b591f'},
+    {'id': 2, 'name': 'Ryan', 'email': 'ryan@example.com',
+        'password': '4062a90a69c6a9007cbb627030e7ee67948cc842632d2c6e1c0cf75c087f0e20'},
+    {'id': 3, 'name': 'Nathan', 'email': 'nathan@example.com',
+        'password': 'a85a433586c7cc7c7a3c0a1b85f9ce78b5c06a2b5af926a4bf1c96fd37d408fc'}
 ]
 
 
@@ -15,13 +21,20 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/users', methods=['DELETE'])
+def clear_users():
+    global users
+    users = []
+    return jsonify("users deleted")
+
+
 @app.route('/users', methods=['POST'])
 def create_user():
-    new_user = {
-        'id': len(users) + 1,
-        'name': request.form['name'],
-        'email': request.form['email']
-    }
+    new_user = request.get_json()
+    new_user['id'] = len(users) + 1
+    password = new_user['password'].encode('utf-8')
+    hashed_password = hashlib.sha256(password).hexdigest()
+    new_user['password'] = hashed_password
     users.append(new_user)
     return jsonify(new_user)
 
@@ -40,4 +53,4 @@ def get_users():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
