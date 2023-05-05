3848650dc576298af3bd8a2c8e15702edb95816b
Matt Keeley <keeley55@me.com>
2023-05-05 15:50:52 -0700

    fixes

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 # Pretend this is a databse :P
 users = [
-    {'id': 0, 'name': 'admin', 'email': 'admin@example.com',
-     'password': '2355d80c210fded577805e0b984810fe6605a51bb790b18d35092121250653ae'},
-    {'id': 1, 'name': 'Matt', 'email': 'matt@example.com',
+    {'id': 1, 'name': 'admin', 'email': 'admin@example.com',
+     'password': 'dbfd43b661440a7725c23e277c62e33277acfbdf7331b56fc5545b928894416d'},
+    {'id': 2, 'name': 'Matt', 'email': 'matt@example.com',
         'password': '50eb299b46fe70fb31b22d195410c0c2b6b959a8beebd8c2c080f428b90b591f'},
-    {'id': 2, 'name': 'Ryan', 'email': 'ryan@example.com',
+    {'id': 3, 'name': 'Ryan', 'email': 'ryan@example.com',
         'password': '4062a90a69c6a9007cbb627030e7ee67948cc842632d2c6e1c0cf75c087f0e20'},
-    {'id': 3, 'name': 'Nathan', 'email': 'nathan@example.com',
+    {'id': 4, 'name': 'Nathan', 'email': 'nathan@example.com',
         'password': 'a85a433586c7cc7c7a3c0a1b85f9ce78b5c06a2b5af926a4bf1c96fd37d408fc'}
 ]
 
@@ -24,7 +24,9 @@ def index():
 @app.route('/users', methods=['DELETE'])
 def clear_users():
     global users
-    users = []
+    users = [
+        {'id': 1, 'name': 'admin', 'email': 'admin@example.com',
+         'password': 'dbfd43b661440a7725c23e277c62e33277acfbdf7331b56fc5545b928894416d'}]
     return jsonify("users deleted")
 
 
